11a969e1e029b241fb59f864da6c2767407839bd
msi <msi@130.190.31.164>
2013-11-29 10:50:25 +0100

    Suppression de jsp inutiles, changement des servlets pour compenser changement des create en edit modifictation de listProd //TODO le buyProd est a finir, avec l'ajout dans le cart,...

--- src/java/web/addCategory.java
+++ src/java/web/addCategory.java
@@ -5,8 +5,6 @@
 package web;
 
 import ejb.CategoryFacade;
-import product.Product;
-import ejb.ProductFacade;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.ejb.EJB;
@@ -14,19 +12,16 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import product.Manufacturer;
 import product.type.Category;
 
 /**
  *
  * @author msi
  */
-public class addProd extends HttpServlet {
+public class addCategory extends HttpServlet {
 
     @EJB
-    private ProductFacade pef;
-    @EJB
-    private CategoryFacade cf;
+    CategoryFacade cf;
 
     /**
      * Processes requests for both HTTP
@@ -40,9 +35,6 @@ public class addProd extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-
-
-
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
         try {
@@ -50,47 +42,41 @@ public class addProd extends HttpServlet {
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Ajout de produit</title>");
+            out.println("<title>Servlet addCategory</title>");
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet addProd at " + request.getContextPath() + "</h1>");
-
+            out.println("<h1>Servlet addCategory at " + request.getContextPath() + "</h1>");
             if (!request.getParameterNames().hasMoreElements()) {
-                out.println("<form name=\"addProduct\" action=\"addProd\" method=\"POST\">");
-                out.println("<label for=\"name\">Product name</label>");
+
+                out.println("<form name=\"addCategory\" action=\"addCategory\" method=\"POST\">");
+                out.println("<label for=\"name\">Category name</label>");
                 out.println("<input id=\"name\" type=\"text\" name=\"name\" value=\"\" size=\"30\" /><br/>");
-                out.println("<label for=\"brand\">Brand</label>");
-                out.println("<input id=\"brand\" type=\"text\" name=\"brand\" value=\"\" size=\"30\" /><br/> ");
-                out.println("<label for=\"Category\">Category</label>");
-
-                out.println("<select id=\"category\" name=\"category\"> ");
-
+                out.println("<label for=\"parent\">Parent</label>");
+                out.println("<select id=\"parent\" type=\"text\" name=\"parent\" value=\"\" size=\"1\">");
+                out.println("<option value=\"null\">Aucun</option>");
+                
                 for (Category c : cf.findAll()) {
                     out.println("<option value=\"" + c.getId() + "\">" + c.getCategorie() + "</option>");
                 }
                 
                 out.println("</select><br />");
-                out.println("<label for=\"price\">Price</label>");
-                out.println("<input id=\"price\" type=\"text\" name=\"price\" value=\"\" size=\"10\" />  <br/>");
-                out.println("<input type=\"submit\" value=\"Submit\" />\n");
+                out.println("<input type=\"submit\" value=\"Submit\" />");
                 out.println("</form>");
             } else {
+                Category c = new Category();
+                c.setCategorie(request.getParameter("name"));
 
-                Product pe = new Product();
-                Manufacturer m = new Manufacturer();
-                m.setName(request.getParameter("brand"));
-                pe.setBrand(m);
-                pe.setName(request.getParameter("name"));
-                pe.setCategorie(cf.find(Long.parseLong(request.getParameter("category"))));
-
-                pe.setPrice(Float.parseFloat(request.getParameter("price")));
-                pef.edit(pe);
-                out.println("Produit " + request.getParameter("name") + " ajouté.");
+                if (!request.getParameter("parent").equals(new String("null"))) {
+                    c.setParent(cf.find(Long.parseLong(request.getParameter("parent"))));
+                    cf.edit(c);
+                }
+                
+                
+                out.println("Catégorie "+request.getParameter("name")+ " ajoutée");
             }
-
-            out.println("</body>");
-            out.println("</html>");
-        } finally {
+                out.println("</body>");
+                out.println("</html>");
+            }  finally {
             out.close();
         }
     }
