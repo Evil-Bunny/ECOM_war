ae5a2c6698af999ac92ade7aeb03dbdbfc45131b
Bousky <nicolas.bouscarle@centraliens-lille.org>
2013-12-05 15:08:33 +0100

    recherche avancée

--- src/java/pages/Search.java
+++ src/java/pages/Search.java
@@ -48,7 +48,7 @@ public class Search extends AbstractPage {
         List<Characteristic> caracteristics = chf.findAll();
         out.println("<form action='.'>");
         out.println("<input type='hidden' name='page' value='Products'/>");
-        out.println("<input type='hidden' name='search' value=''/>");
+        //out.println("<input type='hidden' name='search' value=''/>");
         out.println("<label>Nom du produit<input type='text'/></label>");
         out.println("<label>Marque & Constructeur<select name='manufacturer'>");
         out.println("<option value=''>Choisir une marque</option>");
@@ -70,7 +70,7 @@ public class Search extends AbstractPage {
         out.println("<tr><td><select name='caracName_1'>");
         Collections.sort(caracteristics);
         for (Characteristic c : caracteristics) {
-            out.println("<option>"+c.getName()+"</option>");
+            out.println("<option value='"+c.getId()+"'>"+c.getName()+"</option>");
         }
         out.println("</select></td><td><input type='text' name='caracVal_1'/></td><td><button type='button' onclick='delCarac(this)'>X</button></td></tr>");
         out.println("<tr><td><button type='button' onclick='addCarac()'>+</button></td><td></td></tr>");
