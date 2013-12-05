476107393a6b48d8a7523dab5c2e29ab0e1f3b68
Samy <Samy@147.171.164.228>
2013-12-05 16:35:44 +0100

    liste des produits du panier dans le menu

--- src/java/pages/ViewCart.java
+++ src/java/pages/ViewCart.java
@@ -36,18 +36,28 @@ public class ViewCart extends AbstractPage {
             cart = (Cart) ((Client) session.getAttribute("client")).getCart();
         }
 
-        if (request.getParameter("menu") == null) {
+        if (request.getParameter("total") == null && request.getParameter("menu") == null) {
             out.println("<h2>Panier</h2>");
             for (LineCommand p : cart.getProducts()) {
-                out.println(p.getQuantity() + "x " + p.getProduct().getName() + ": " +String.format("<div class='price'>%.2f &euro;</div>", p.storePrice()) + "<br/>");
+                out.println(p.getQuantity() + "x " + p.getProduct().getName() + ": " + String.format("<div class='price'>%.2f &euro;</div>", p.storePrice()) + "<br/>");
             }
-            out.println("Total: " + String.format("<div class='price'>%.2f &euro;</div>", cart.getTotal()) );
-        } else {
+            out.println("Total: " + String.format("<div class='price'>%.2f &euro;</div>", cart.getTotal()));
+        } else if (request.getParameter("menu") == null) {
             Integer total = 0;
             for (LineCommand p : cart.getProducts()) {
                 total += p.getQuantity();
             }
             out.print(total);
+        } else if (request.getParameter("total") == null) {
+            out.println("<ul>");
+            for (LineCommand p : cart.getProducts()) {
+                out.println("<li><a href='?page=Product&amp;id="+p.getProduct().getId()+"'>" + p.getProduct().getName() + "</a></li>");
+            }
+            out.println("</ul>");
+//						<li><a href="toto" title="Toto">Toto</a></li>
+//						<li><a href="tata" title="Tata">Tata</a></li>
+//						<li><a href="titi" title="Titi et Gros Minet sont sur un bateau">Titi et Gros Minet sont sur un bateau</a></li>
+//					</ul>        }
         }
     }
 }
